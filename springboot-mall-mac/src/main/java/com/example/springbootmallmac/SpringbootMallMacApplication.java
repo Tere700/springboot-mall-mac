d1feb14df9ef3747246518d138c4146b58f658b5
teresaChen <tere06ccc@gmail.com>
2024-05-30 12:06:17 +0800

    初始化 Spring Boot Project

---
+++ springboot-mall-mac/src/main/java/com/example/springbootmallmac/SpringbootMallMacApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmallmac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMallMacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMallMacApplication.class, args);
+    }
+
+}
