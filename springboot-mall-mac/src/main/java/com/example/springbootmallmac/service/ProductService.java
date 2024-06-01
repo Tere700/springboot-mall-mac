c93b4e88c023055a3a14132249628a0bdf3f423d
teresaChen <tere06ccc@gmail.com>
2024-06-01 11:45:33 +0800

    商品功能 - 實作修改商品

--- springboot-mall-mac/src/main/java/com/example/springbootmallmac/service/ProductService.java
+++ springboot-mall-mac/src/main/java/com/example/springbootmallmac/service/ProductService.java
@@ -6,4 +6,5 @@ import com.example.springbootmallmac.model.Product;
 public interface ProductService {
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
+    void updateProduct(Integer productId, ProductRequest productRequest);
 }
