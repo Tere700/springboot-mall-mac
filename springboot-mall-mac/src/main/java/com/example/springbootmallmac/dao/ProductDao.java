c58db54800eb5e695be1fb807b776061666819bc
teresaChen <tere06ccc@gmail.com>
2024-06-02 13:25:32 +0800

    商品功能 - 實作查詢商品列表(基礎實作)

--- springboot-mall-mac/src/main/java/com/example/springbootmallmac/dao/ProductDao.java
+++ springboot-mall-mac/src/main/java/com/example/springbootmallmac/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.example.springbootmallmac.dao;
 import com.example.springbootmallmac.dto.ProductRequest;
 import com.example.springbootmallmac.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId, ProductRequest productRequest);
--- springboot-mall-mac/src/main/java/com/example/springbootmallmac/service/ProductService.java
+++ springboot-mall-mac/src/main/java/com/example/springbootmallmac/service/ProductService.java
@@ -3,7 +3,11 @@ package com.example.springbootmallmac.service;
 import com.example.springbootmallmac.dto.ProductRequest;
 import com.example.springbootmallmac.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId, ProductRequest productRequest);
